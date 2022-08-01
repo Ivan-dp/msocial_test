9c6be0e07a0945373d4e8f479ffaab0d1c78d5f5
Ivan-dp <ivan-dp@bk.ru>
2022-08-02 00:16:20 +0300

    Created a project. Basic webpack setup. Test layout

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    historyApiFallback: true,
+    // contentBase: path.resolve(__dirname, "./dist"),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  entry: {
+    // index: path.resolve(__dirname, "./src/index.js"),
+    index: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.pug",
+      chunks: ["index"],
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+
+      // изображения
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+
+      //   {
+      //     test: /\.(jpg|png)$/,
+      //     use: {
+      //       loader: 'url-loader',
+      //     },
+      //   },
+
+      // шрифты и SVG
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+
+      // CSS, PostCSS, Sass
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+
+      // pug
+      {
+        test: /\.pug$/,
+        use: {
+          loader: "pug-loader",
+          options: {
+            pretty: true,
+          },
+        },
+      },
+    ],
+  },
+};
