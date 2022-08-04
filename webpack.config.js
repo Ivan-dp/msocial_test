a4850dc79e2611d15ee87c27574be4766d5d261c
Ivan-dp <ivan-dp@bk.ru>
2022-08-04 23:03:27 +0300

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "[name].bundle.js",
+    filename: "[name].[hash]bundle.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
