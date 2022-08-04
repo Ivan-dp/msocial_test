d8f7025a617cb73bd75ca182eb84f6d391e69b51
Ivan-dp <ivan-dp@bk.ru>
2022-08-04 15:18:31 +0300

    modified styles for screen size 641-960px

--- src/blocks/action-goods/action-goods.js
+++ src/blocks/action-goods/action-goods.js
@@ -10,23 +10,23 @@ const swiper = new Swiper(".swiper", {
   loop: true,
   breakpoints: {
     // when window width is >= 320px
+    // 320: {
+    //   slidesPerView: 1,
+    //   // spaceBetween: 20
+    // },
     320: {
-      slidesPerView: 1,
-      // spaceBetween: 20
-    },
-    480: {
       slidesPerView: 2,
-      spaceBetween: 20,
+      spaceBetween: 10,
     },
     // when window width is >= 480px
-    768: {
+    640: {
       slidesPerView: 3,
-      spaceBetween: 30,
+      spaceBetween: 10,
     },
     // when window width is >= 640px
-    1024: {
+    960: {
       slidesPerView: 4,
-      spaceBetween: 40,
+      spaceBetween: 20,
     },
   },
   spaceBetween: 10,
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,3 +4,11 @@ import "../../blocks/action-goods/action-goods.js";
 console.log("It works!");
 
 console.log(window);
+
+var menuButton = document.querySelector(".menu-button");
+var menu = document.querySelector(".menu-drop");
+
+menuButton.addEventListener("click", function () {
+  menuButton.classList.toggle("menu-button-active");
+  menu.classList.toggle("menu-drop-active");
+});
