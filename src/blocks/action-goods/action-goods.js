4958d3c390ae93b04788285fea0142f5dc793a67
Ivan-dp <ivan-dp@bk.ru>
2022-08-04 05:46:05 +0300

    modified media queries for 3 diapasons

--- src/blocks/action-goods/action-goods.js
+++ src/blocks/action-goods/action-goods.js
@@ -8,7 +8,27 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
-  slidesPerView: 4,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      // spaceBetween: 20
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
   spaceBetween: 10,
   modules: [Navigation, Pagination],
 
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,3 +2,5 @@ import "./index.scss";
 import "../../blocks/action-goods/action-goods.js";
 
 console.log("It works!");
+
+console.log(window);
