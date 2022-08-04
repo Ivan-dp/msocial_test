8d445ee774113d2619c7e0ef59b5d7756bf5851e
Ivan-dp <ivan-dp@bk.ru>
2022-08-04 22:39:12 +0300

    modified links of .nav

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,3 +2,25 @@ import "./index.scss";
 import "../../blocks/action-goods/action-goods.js";
 
 console.log("It works!");
+
+console.log(window);
+
+var menuButton = document.querySelector(".menu-button");
+var menu = document.querySelector(".menu-drop");
+
+menuButton.addEventListener("click", function () {
+  menuButton.classList.toggle("menu-button-active");
+  menu.classList.toggle("menu-drop-active");
+});
+
+// var link = menu.querySelectorAll(".nav > a");
+// console.log(link);
+// const func = () => {
+//   for (let i = 0; i < link.length; i++) {
+//     link[i].addEventListener("click", function () {
+//       menuButton.classList.toggle("menu-button");
+//     });
+//   }
+// };
+
+// func();
