f14bbfa8d07c60e63e21342a93e3586e8c3a17b3
Ivan-dp <ivan-dp@bk.ru>
2022-08-22 15:05:47 +0300

    write a function for toggle class of popup

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -12,3 +12,19 @@ menuButton.addEventListener("click", function () {
   menuButton.classList.toggle("menu-button-active");
   menu.classList.toggle("menu-drop-active");
 });
+
+var logInButton = document.querySelector(".log-in-btn");
+var logIn = document.querySelector(".popup-log-in");
+
+var signInButton = document.querySelector(".sign-in-btn");
+var signIn = document.querySelector(".popup-sign-in");
+
+logInButton.addEventListener("click", function () {
+  logInButton.classList.toggle("log-in-btn-active");
+  logIn.classList.toggle("popup-log-in-active");
+});
+
+signInButton.addEventListener("click", function () {
+  signInButton.classList.toggle("sign-in-btn-active");
+  signIn.classList.toggle("popup-sign-in-active");
+});
