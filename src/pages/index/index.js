660e16dc46ba78a526fa06849376d1eece0488d2
Ivan Litovchenko <54504811+Ivan-dp@users.noreply.github.com>
2022-08-22 19:38:14 +0300

    Popup (#18)
    * write a function for toggle class of popup
    
    * created popup block and included it into project. Modified index.js for popup__close button correct work
    
    * modified styles of popup block for dark background color

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -12,3 +12,23 @@ menuButton.addEventListener("click", function () {
   menuButton.classList.toggle("menu-button-active");
   menu.classList.toggle("menu-drop-active");
 });
+
+var logInButton = document.querySelector(".log-in-btn");
+var popup = document.querySelector(".popup");
+var popupClose = document.querySelector(".popup__close");
+
+var signInButton = document.querySelector(".sign-in-btn");
+
+logInButton.addEventListener("click", function () {
+  logInButton.classList.toggle("log-in-btn-active");
+  popup.classList.toggle("popup-active");
+});
+
+popupClose.addEventListener("click", function () {
+  popup.classList.remove("popup-active");
+});
+
+signInButton.addEventListener("click", function () {
+  signInButton.classList.toggle("sign-in-btn-active");
+  popup.classList.toggle("popup-active");
+});
