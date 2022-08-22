8d7af75d73c66e181eabefa462658bf9c96b846f
Ivan Litovchenko <54504811+Ivan-dp@users.noreply.github.com>
2022-08-23 01:46:45 +0300

    Optimization (#23)
    * connected all needed formats of fonts. Created mixin for fonts.
    
    * applied font mixins wherever the font is used. Created new mixin - Flex. Applied it wherever the flex property is used
    
    * created variables.scss. Used variables wherever the primary colors were used
    
    * optimized scss code
    
    * modified links of .nav
    
    * some fixed
    
    * optimized format and size of png files
    
    * installed autoprefixer
    
    * documenting comments added to the code

--- src/blocks/action-goods/action-goods.js
+++ src/blocks/action-goods/action-goods.js
@@ -1,43 +1,49 @@
-import Swiper, { Navigation, Pagination } from "swiper";
+import Swiper, { Navigation, Pagination } from "swiper"; // Импортирую swiper в js файл
+
+// Подключаю стили swiper
 
 import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
 
+// Инициализирую плагин swiper в блоке .swiper
+
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
+  // Устанавливаю положение swiper
+
   direction: "horizontal",
+
+  // Устанавливаю повторяющуюся промотку sliders
+
   loop: true,
+
+  // Устанавливаю параметры для разных размеров экранов
+
   breakpoints: {
-    // when window width is >= 320px
-    // 320: {
-    //   slidesPerView: 1,
-    //   // spaceBetween: 20
-    // },
     320: {
       slidesPerView: 2,
       spaceBetween: 25,
     },
-    // when window width is >= 480px
     640: {
       slidesPerView: 3,
       spaceBetween: 10,
     },
-    // when window width is >= 640px
     960: {
       slidesPerView: 4,
       spaceBetween: 20,
     },
   },
-  spaceBetween: 10,
-  modules: [Navigation, Pagination],
 
-  // If we need pagination
+  // Устанавливаю расстояние между slides
+
+  spaceBetween: 10,
+
+  // Использую кнопки навигации и пагинацию
+
+  modules: [Navigation, Pagination],
   pagination: {
     el: ".swiper-pagination",
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
